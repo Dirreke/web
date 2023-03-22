027a6048f136d39d8788acce1ee3df253b0e418b
Swikriti Tripathi <swikriti808@gmail.com>
2023-03-22 13:04:25 +0545

    implement list members step

--- tests/e2e/cucumber/steps/ui/adminSettings.ts
+++ tests/e2e/cucumber/steps/ui/adminSettings.ts
@@ -434,37 +434,38 @@ Then(
 )
 
 When(
-    '{string} lists the members of project space {string} using a sidebar panel',
-    async function (this:World, stepUser: string, key: string): Promise<void> {
-      const { page } = this.actorsEnvironment.getActor({ key: stepUser })
-      const spacesObject = new objects.applicationAdminSettings.Spaces({ page })
-      await spacesObject.openPanel({key})
-      await spacesObject.openActionSideBarPanel({action:'SpaceMembers'})
-
-    }
+  '{string} lists the members of project space {string} using a sidebar panel',
+  async function (this: World, stepUser: string, key: string): Promise<void> {
+    const { page } = this.actorsEnvironment.getActor({ key: stepUser })
+    const spacesObject = new objects.applicationAdminSettings.Spaces({ page })
+    await spacesObject.openPanel({ key })
+    await spacesObject.openActionSideBarPanel({ action: 'SpaceMembers' })
+  }
 )
 
-
 Then(
-    '{string} should see the following users in the webUI',
-    async function (this:World, stepUser: string,stepTable: DataTable): Promise<void>{
-      const { page } = this.actorsEnvironment.getActor({ key: stepUser })
-      const spacesObject = new objects.applicationAdminSettings.Spaces({ page })
-      let expected = null
-      for(const info of stepTable.hashes()){
-        switch (info.role){
-          case 'manager':
-            expected = await spacesObject.listMembers({filter:'managers'})
-            break
-          case 'editor':
-            expected = await spacesObject.listMembers({filter:'editor'})
-            break
-          case 'viewer':
-            expected = await spacesObject.listMembers({filter:'viewer'})
-            break
-          default:
-            throw new Error(`'${info.role}' role not implemented`)
-        }
+  '{string} should see the following users in the webUI',
+  async function (this: World, stepUser: string, stepTable: DataTable): Promise<void> {
+    const { page } = this.actorsEnvironment.getActor({ key: stepUser })
+    const spacesObject = new objects.applicationAdminSettings.Spaces({ page })
+    let actualValues = null
+    for (const info of stepTable.hashes()) {
+      switch (info.role) {
+        case 'manager':
+          actualValues = await spacesObject.listMembers({ filter: 'managers' })
+          expect(actualValues).toContain(info.user)
+          break
+        case 'editor':
+          actualValues = await spacesObject.listMembers({ filter: 'editors' })
+          expect(actualValues).toContain(info.user)
+          break
+        case 'viewer':
+          actualValues = await spacesObject.listMembers({ filter: 'viewers' })
+          expect(actualValues).toContain(info.user)
+          break
+        default:
+          throw new Error(`'${info.role}' role not implemented`)
       }
     }
+  }
 )
